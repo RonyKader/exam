a4270d0a4ce7f1a992077142cbe6367bd09d6af8
RonyKader <tarikul.islam@chefonline.co.uk>
2016-10-06 14:04:26 +0100

    change config page

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
 define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "db_name");
+define("DB_USER", "w3zoncm_tarik");
+define("DB_PASS", "RonyKader9666");
+define("DB_NAME", "w3zoncm_elspress");
